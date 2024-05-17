2de95f40fbdd1c636bcec77492cb52b07c6e75e8
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2024-05-17 20:52:19 +0300

    Fermi-Dirac statistics with single conduction level: draft

---
+++ uploads/models/physics-models/fermi-dirac-statistics/single-conduction-level.js
@@ -0,0 +1,151 @@
+/* global jStat:readable, Random: readable, plotlyPlot:readable, setTimeout:readable */
+
+const my_parse_float = (val) => parseFloat(("" + val).replace(",", "."));
+
+let rng = new Random();
+
+let update_interval = 10;
+let continue_flag = false;
+
+const N_LEVELS = 99; // excludes conduction band!
+const N_PARTICLES = 50;
+const N_SPACES = N_LEVELS - N_PARTICLES;
+const RUN_STEPS = N_PARTICLES;
+const ENERGIES = Array(N_LEVELS)
+    .fill(null)
+    .map((v, i) => i + 1);
+
+let temperature = 1;
+let states = Array(N_LEVELS).fill(0);
+let free_particles = 0;
+let steps_done = 0;
+let free_history = 0;
+let state_history = Array(states.length).fill(0);
+
+let prob_plot = new plotlyPlot("probPlot", ["E", "P(E)"], [10, 15, 40, 60]);
+let start_btn = document.getElementById("start");
+let resume_btn = document.getElementById("resume");
+
+const COLORS = ["#c11", "#444"];
+
+function get_theory(T) {
+    let norm = 1 - free_history / N_PARTICLES;
+    return Array(ENERGIES.length)
+        .fill(null)
+        .map((v, i) => {
+            return norm / (1 + Math.exp((i + 1 - N_PARTICLES) / T));
+        });
+}
+
+function plot() {
+    let theory = get_theory(temperature);
+    let data = state_history.map((v) => v / steps_done);
+    prob_plot.update(
+        [ENERGIES, ENERGIES],
+        [data, theory],
+        ["markers", "lines"],
+        COLORS
+    );
+}
+
+function initialize() {
+    temperature = Math.pow(
+        10,
+        my_parse_float(document.getElementById("temperature").value)
+    );
+    let tmp = Array(N_LEVELS)
+        .fill(null)
+        .map(() => rng.random());
+    let cutoff = jStat.percentile(tmp, N_PARTICLES / N_LEVELS);
+    states = tmp.map((v) => (v < cutoff ? 1 : 0));
+    steps_done = 1;
+    free_particles = 0;
+    state_history = states.slice();
+    free_history = free_particles;
+}
+
+function step() {
+    let particle_id = Math.ceil(rng.uniform(0, N_PARTICLES));
+    let loc_particle = -1;
+    let loc_space = -1;
+    if (particle_id > N_PARTICLES - free_particles) {
+        // free particle is selected
+        let space_id = Math.ceil(rng.uniform(0, N_SPACES + free_particles));
+        let current_loc = 0;
+        while (loc_space < 0) {
+            if (states[current_loc] == 0) {
+                space_id -= 1;
+                if (space_id == 0) {
+                    loc_space = current_loc;
+                }
+            }
+            current_loc += 1;
+        }
+        loc_particle = N_LEVELS + 1;
+    } else {
+        // non-free particle is selected
+        let current_loc = 0;
+        while (loc_particle < 0) {
+            if (states[current_loc] == 1) {
+                particle_id -= 1;
+                if (particle_id == 0) {
+                    loc_particle = current_loc;
+                }
+            }
+            current_loc += 1;
+        }
+        loc_space = N_LEVELS + 1;
+    }
+
+    let jump_prob = Math.exp((loc_particle - loc_space) / temperature);
+    if (rng.random() < jump_prob) {
+        if (loc_space == N_LEVELS + 1) {
+            free_particles = free_particles + 1;
+            states[loc_particle] = 0;
+        }
+        if (loc_particle == N_LEVELS + 1) {
+            free_particles = free_particles - 1;
+            states[loc_space] = 1;
+        }
+    }
+}
+
+function run() {
+    for (let i = 0; i < RUN_STEPS; i += 1) {
+        step();
+    }
+    free_history =
+        (free_history * steps_done + free_particles) / (steps_done + 1);
+    steps_done = steps_done + 1;
+    state_history = state_history.map((v, i) => v + states[i]);
+    plot();
+    if (continue_flag) {
+        setTimeout(run, update_interval);
+    }
+}
+
+// events
+start_btn.addEventListener("click", () => {
+    start_btn.disabled = true;
+    resume_btn.innerHTML = "Stop";
+
+    initialize();
+
+    continue_flag = true;
+    setTimeout(run, update_interval);
+
+    resume_btn.disabled = false;
+});
+resume_btn.addEventListener("click", () => {
+    resume_btn.disabled = true;
+    continue_flag = !continue_flag;
+    if (continue_flag) {
+        setTimeout(run, update_interval);
+        resume_btn.innerHTML = "Stop";
+    } else {
+        resume_btn.innerHTML = "Resume";
+    }
+    start_btn.disabled = continue_flag;
+    resume_btn.disabled = false;
+});
+resume_btn.disabled = true;
