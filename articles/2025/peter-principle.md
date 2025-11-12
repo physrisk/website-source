fdb34cb551410e0ae57bbbcbe40a0b2322751be6
Aleksejus Kononovicius <aleksejus.kononovicius@gmail.com>
2025-11-12 15:43:58 +0200

    Peter principle: draft

---
+++ articles/2025/peter-principle.md
@@ -0,0 +1,226 @@
+Title: Peter principle
+Date: 2025-11-18 08:00
+Author: Aleksejus Kononovicius
+Tags: agent-based models, interactive, economics, Peter principle
+Slug: peter-principle
+Status: draft
+Image_url: uploads/2025/peter-principle.png
+
+[Last time]({filename}/articles/2025/ritvikmath-and-the-peter-principle.md)
+I have shared a video about the Peter principle, which states that "in a
+hierarchy, every employee tends to rise to their level of incompetence."
+Actually, I have heard about this principle before in the scientific
+context. Prior to the video, I have heard about this principle from a group
+of Italian physicists, economists and mathematicians had working on a
+research project about benefits of random strategies [cite
+id="Biondo2013JStatPhys"]. On of their works was about the [Peter
+principle](/tag/peter-principle/). So, lets explore!
+<!--more-->
+
+## Model
+
+First we need to be the hierarchy. Let us build a simple hierarchical tree
+network. This we start with the top node, which corresponds to CEO or owner.
+That node is connected to three branches (their juniors). In turn, each of
+the junior nodes have their own three juniors (three branches). Formation of
+hierarchical relationships (or branching of the network) repeats until the
+bottom level. The "bottom" nodes are low-rank recruits who do not have any
+juniors. In the app below we have six hierarchical levels including the CEO.
+
+Our choice of the branching factor (how many branches stem from the top
+parent nodes) and the number of generations (how many levels there are) is
+purely arbitrary. These choices shouldn't have any qualitative impact on the
+results. While we make some arbitrary choices here, we still follow the
+general logic outlined in [cite id="Biondo2013JStatPhys"].
+
+To make the simulated workplace realistic, each worker (node) should have
+competence and age attributes. Competence is clearly important for our
+purposes, while age provides an additional mechanism influencing when
+workers leave. Specifically, workers will leave the simulated workplace
+either upon retirement or if they become incompetent.
+
+### Worker retirement and layoffs
+
+When a worker leaves the simulated workplace, they will be immediately
+replaced by one of their juniors. If the worker is of the lowest rank, they
+will be replace by a random generated worker. Random worker will start with
+a random age between \\\( 18 \\\) and \\\( 25 \\\) (uniformly distributed),
+and a random competence score between \\\( 0 \\\) and \\\( 1 \\\) (uniformly
+distributed). When replacing their supervisor, the node will change their
+competence score according to the following formula,
+
+\begin{equation}
+    C(t+1) = \rho \cdot C(t) + ( 1 - \rho ) \cdot \xi .
+\end{equation}
+
+In the above \\\( \xi \\\) stands for the uniform random variate, while \\\(
+\rho \\\) is the model parameter having the meaning of competence
+correlation. If the competence of the worker falls below a threshold value
+(we have set it to \\\( 0.1 \\\) in the app below), then they will be laid
+off during the next simulation step. As soon as worker gets laid off, new
+wave of promotions is triggered.
+
+While the competence changes only on promotion, the age gets incremented
+during each simulation step. If the worker reaches retirement age
+(arbitrarily set to \\\( 65 \\\) in the app below), they will immediately
+retire. Once they retire, they will trigger promotions.
+
+### Promotion algorithms
+
+The model is not fully defined without a promotion algorithm. Without it we
+wouldn't know whom of the three juniors to promote. The app below
+implements four different approaches.
+
+* **Pick best.** Junior with greatest competence is selected.
+* **Pick worst.** Junior with lowest competence is selected.
+* **Random pick.** Random junior is selected.
+* **Pick best or worst.** There is an equal change that best or worst junior
+  will be selected.
+
+### Calculating company efficiency
+
+After all layoffs, retirements and promotions have been dealt with, company
+efficiency is calculated. First of all, competence across each level is
+being averaged. Then weighted average across the levels is calculated.
+Mathematically company efficiency is defined as
+
+\begin{equation}
+E = \sum\_{i=0}^{L-1} \left( \frac{w\_i}{N\_i} \sum\_{j=0}^{N\_i-1} C^{(i,j)} \right) .
+\end{equation}
+
+In the above \\\( L \\\) is the number of levels in the organization, \\\(
+w\_i \\\) is the weight (relative importance) of the \\\( i \\\)-th level,
+\\\( N\_i \\\) is the number workers at the \\\( i \\\)-th level, \\\(
+C^{(i,j)} \\\) is the current competence of \\\( j \\\)-th worker on the
+\\\( i \\\)-th level. Note that with this notation zero-th level corresponds
+to the CEO, while \\\( L-1 \\\)-st level contains workers of the lowest
+rank.
+
+In the app below we have \\\( L = 6 \\\) and \\\( N\_i = 3^i \\\). We
+calculate the importance of each hierarchical level as follows,
+
+\begin{equation}
+w\_i = (1 + \Delta w)^{L-i-1} = (1 + \Delta w)^{5-i} .
+\end{equation}
+
+Here, \\\( \Delta w \\\) is a parameter that controls how the importance of
+hierarchical level changes with rank. A positive value means higher-ranking
+workers are given more weight, while a negative value means their importance
+decreases. The app allows you to choose \\\( \Delta w \in \{-0.8, -0.5,
+-0.25, 0, 0.25, 0.5, 1.0\} \\\). Notably, choosing an extreme \\\( \Delta w
+\\\) value, may have a qualitative impact on the results as choosing \\\(
+\Delta w < \frac{B-1}{B} \\\) (where \\\( B \\\) is the branching factor)
+makes higher rank worker contribute less than a \\\( B \\\) lower rank
+workers.
+
+## Results
+
+### When competence is transferred between the ranks
+
+The results when common sense hypothesis (as defined in [cite
+id="Biondo2013JStatPhys"]; we refer to this hypothesis as "Transfer"
+hypothesis) holds, are trivial. "Pick best" promotion strategy produces the
+best efficiency, while "Pick worst" produces the worst outcomes.
+
+!['Pick best' promotion strategy results under competence transfer
+hypothesis.]({static}/uploads/2025/peter-principle-transfer-increasing-best.png
+"'Pick best' promotion strategy results under competence transfer
+hypothesis.")
+
+!['Pick worst' promotion strategy results under competence transfer
+hypothesis.]({static}/uploads/2025/peter-principle-transfer-increasing-worst.png
+"'Pick worst' promotion strategy results under competence transfer
+hypothesis.")
+
+On the other hand "Pick random" promotion strategy produces company
+efficiencies spread around the mean random competence, \\\( E \approx
+\langle C \rangle = 0.55 \\\).
+
+!['Random pick' promotion strategy results under competence transfer
+hypothesis.]({static}/uploads/2025/peter-principle-transfer-increasing-random.png
+"'Random pick' promotion strategy results under competence transfer
+hypothesis.")
+
+"Pick best or worst" promotion strategy produces bimodal distribution
+centered around the mean random competence, \\\( \langle C \rangle = 0.55
+\\\).
+
+!['Pick best or worst' promotion strategy results under competence transfer
+hypothesis.]({static}/uploads/2025/peter-principle.png "'Pick best or worst'
+promotion strategy results under competence transfer hypothesis.")
+
+### When competence required for each rank is uncorrelated
+
+The results when the [Peter principle](/tag/peter-principle/) hypothesis (as
+defined in [cite id="Biondo2013JStatPhys"]; we refer to this hypothesis as
+"Randomization" hypothesis) holds, are less trivial. In this case, "Pick
+worst" promotion strategy yields the best company efficiency.
+
+Though the result is unexpected only at the first glance. Namely,
+"Randomization" hypothesis implies that competences required at each
+hierarchical level are unrelated. "Promotion" in this case means that a
+worker gets another opportunity to find something they are good at. When we
+"promote" the worst worker, we effectively remove bad competence score from
+the pool and draw completely new competence score, which is likely to be
+better, because we compare the new sampled value with a bad value.
+
+!['Pick best' promotion strategy results under competence randomization
+hypothesis.]({static}/uploads/2025/peter-principle-randomization-constant-best.png
+"'Pick best' promotion strategy results under competence randomization
+hypothesis.")
+
+!['Pick worst' promotion strategy results under competence randomization
+hypothesis.]({static}/uploads/2025/peter-principle-randomization-constant-worst.png
+"'Pick worst' promotion strategy results under competence randomization
+hypothesis.")
+
+Note that the size of the effect depends on how the level of importance
+scales with increasing rank. We have chosen to show this effect when the
+importance of each rank is identical, i.e., \\\( \Delta w = 0 \\\).
+
+### What I have found interesting
+
+In research it is a commonly said that most work is being done by doctoral
+students and postdocs, while principal investigators (tenured professors)
+live off their students. This observation implies that the level of
+importance of each rank could decrease. What happens then? Well, promoting
+the best still seems to make sense!
+
+!['Pick best' promotion strategy when lower ranks have greater
+importance.]({static}/uploads/2025/peter-principle-transfer-decreasing-best.png
+"'Pick best' promotion strategy when lower ranks have greater importance.")
+
+This is likely the result of the fact that the lowest rank consists of a
+large number of random individuals. While, the worst of them remain at the
+lowest rank through out their worklife, a lot of them are constantly being
+replace by new randomly generated recruits. Thus their average competence
+will not be far of the average random competence, \\\( \langle C \rangle =
+0.55 \\\).
+
+Note that the figure above was obtained with \\\( \Delta w = -0.5 \\\). If
+we set \\\( \Delta w = -0.8 \\\) instead, then "Pick best" will no longer
+produce the better outcome. This is because with \\\( \Delta w = -0.8 \\\)
+(which notably less than \\\( \frac{2}{3} \\\)) most total work is actually
+done by the lowest ranked workers (each higher level does less and less of
+the total work).
+
+## Interactive app
+
+Lots of things were already covered in the text above. So you can just jump
+straight into the app to explore the simulated workplace. Though, there is
+still one thing that remains to be explained. It is the upper left plot.
+This plot shows the hierarchical structure of the simulated workplace and
+how the competence is spread out across the ranks. Each individual agent
+(rectangular block) is colored based on their competence attribute. They are
+colored blue (cold) if their competence is low, and red (hot) if it is high.
+The width of each block depends on the number of coworkers of that
+hierarchical rank, while the height of each block indicates the relative
+importance of each hierarchical rank.
+
+[html5-interactive width="520" height="530" mode="iframe"
+url="/uploads/models/economics/peter-principle/index.html"]
+
+Note that, if you change any of the model parameters and press "Continue"
+button, then the model parameters will be reset to their values used in the
+simulation. To change the model parameter values you will need to press
+"Start" button and start the new simulation.
---
+++ uploads/models/economics/peter-principle/index.js
@@ -0,0 +1,413 @@
+const my_parse_float = (val) => parseFloat(("" + val).replace(",", "."));
+
+let time_plot = new plotlyPlot(
+    "timeSeries",
+    ["time", "company efficiency"],
+    [10, 10, 40, 60]
+);
+let competence_history = [];
+let current_step = 0;
+const MAX_HISTORY = 300;
+
+let histogram_plot = new plotlyPlot(
+    "histogramPlot",
+    ["company efficiency", "frequency"],
+    [10, 10, 40, 60]
+);
+const HISTOGRAM_BINS = 31;
+let histogram = Array(HISTOGRAM_BINS).fill(0);
+const HISTOGRAM_BIN_SIZE = 1.0 / (HISTOGRAM_BINS - 1);
+const HISTOGRAM_VALS = histogram.map((v, i) => i * HISTOGRAM_BIN_SIZE);
+
+let continue_flag = false;
+const STEPS_PER_FRAME = 5;
+const FRAME_INTERVAL = 30;
+
+const LEVELS = 6;
+const BRANCHING = 3;
+let _level_weights = [];
+let _total_level_weight = 0;
+
+const START_MIN_AGE = 18;
+const START_MAX_AGE = 25;
+const RETIREMENT_AGE = 65;
+
+const required_competence = 0.1;
+let competence_correlation = 1.0;
+const COMPETENCE_CORRELATION_VALS = [0.9, 0];
+let promotion_algorithm = 0;
+let continue_multiplier = 0;
+let continue_promotion_algorithm = 0;
+let continue_competence_hypothesis = 0;
+
+let hierarchy_canvas = document.getElementById("hierarchyPlot");
+const HIERARCHY_BLOCK_WIDTH = Math.floor(
+    hierarchy_canvas.width / Math.pow(BRANCHING, LEVELS - 1)
+);
+const HIERARCHY_LEFT_PAD = Math.floor(
+    (hierarchy_canvas.width -
+        HIERARCHY_BLOCK_WIDTH * Math.pow(BRANCHING, LEVELS - 1)) /
+        2
+);
+let _hierarchy_block_heights = [];
+let _hierarchy_block_starts = [];
+let _hierarchy_top_pad = 0;
+
+let hierarchy = {};
+
+function randomize_age(full = false) {
+    if (!full) {
+        return START_MIN_AGE + (START_MAX_AGE - START_MIN_AGE) * Math.random();
+    }
+    return START_MIN_AGE + (RETIREMENT_AGE - START_MIN_AGE) * Math.random();
+}
+
+function randomize_competence(previous = 0, correlation = 0) {
+    let noise = Math.random();
+    return correlation * previous + (1.0 - correlation) * noise;
+}
+
+function initialize_agent(full_age_rng = false) {
+    return {
+        age: randomize_age(full_age_rng),
+        competence: randomize_competence(),
+    };
+}
+
+function update_level_weights(multiplier) {
+    _level_weights = Array(LEVELS)
+        .fill(null)
+        .map((v, i) => Math.pow(1 + multiplier, LEVELS - i - 1));
+    _total_level_weight = _level_weights.reduce((c, v) => c + v, 0);
+    _hierarchy_block_heights = (() => {
+        const default_size = Math.round(
+            hierarchy_canvas.height / _total_level_weight
+        );
+        return _level_weights.map((v) =>
+            Math.max(Math.floor(default_size * v), 1)
+        );
+    })();
+    _hierarchy_block_starts = (() => {
+        const cum_sum = (
+            (sum) => (value) =>
+                (sum += value)
+        )(0);
+        return [0, ..._hierarchy_block_heights.map(cum_sum)];
+    })();
+    _hierarchy_top_pad = Math.floor(
+        (hierarchy_canvas.height -
+            _hierarchy_block_starts[_hierarchy_block_starts.length - 1]) /
+            2
+    );
+    if (-_hierarchy_top_pad + 1 >= _hierarchy_block_heights[0]) {
+        _hierarchy_top_pad = 0;
+    }
+}
+
+function update_level_multiplier(override = null) {
+    let level_multiplier = 0;
+    if (override === null) {
+        level_multiplier = my_parse_float(level_multiplier_input.value);
+    } else {
+        level_multiplier = override;
+        level_multiplier_input.value = override;
+    }
+    update_level_weights(level_multiplier);
+    return level_multiplier;
+}
+
+function restore_simulation_values() {
+    update_level_multiplier(continue_multiplier);
+
+    promotion_algorithm = continue_promotion_algorithm;
+    promotion_algorithm_input.value = promotion_algorithm;
+
+    competence_correlation =
+        COMPETENCE_CORRELATION_VALS[continue_competence_hypothesis];
+    competence_hypothesis_input.value = continue_competence_hypothesis;
+}
+
+function initialize() {
+    let competence_hypothesis = parseInt(competence_hypothesis_input.value);
+    competence_correlation = COMPETENCE_CORRELATION_VALS[competence_hypothesis];
+
+    promotion_algorithm = parseInt(promotion_algorithm_input.value);
+
+    continue_multiplier = update_level_multiplier();
+    continue_promotion_algorithm = promotion_algorithm;
+    continue_competence_hypothesis = competence_hypothesis;
+
+    hierarchy = {};
+    for (let level_id = 0; level_id < LEVELS; level_id += 1) {
+        hierarchy[`level_${level_id}`] = Array(Math.pow(BRANCHING, level_id))
+            .fill(null)
+            .map(() => initialize_agent(true));
+    }
+    current_step = 0;
+    competence_history = [];
+
+    histogram = Array(HISTOGRAM_BINS).fill(0);
+}
+
+function replace_agent(level_id, agent_id) {
+    if (level_id == LEVELS - 1) {
+        // at the bottom level recruit new person
+        hierarchy[`level_${level_id}`][agent_id] = initialize_agent();
+        return;
+    }
+    const pick_best = (arr) => {
+        let best_idx = 0;
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i]["competence"] > arr[best_idx]["competence"]) {
+                best_idx = i;
+            }
+        }
+        return best_idx;
+    };
+    const pick_worst = (arr) => {
+        let best_idx = 0;
+        for (let i = 1; i < arr.length; i++) {
+            if (arr[i]["competence"] < arr[best_idx]["competence"]) {
+                best_idx = i;
+            }
+        }
+        return best_idx;
+    };
+    const pick_random = (arr) => {
+        return Math.floor(Math.random() * arr.length);
+    };
+    const pick_best_or_worst = (arr) => {
+        let r = Math.random();
+        if (r < 0.5) {
+            return pick_best(arr);
+        }
+        return pick_worst(arr);
+    };
+    const picker_fn = [pick_best, pick_worst, pick_random, pick_best_or_worst][
+        promotion_algorithm
+    ];
+
+    let other_level = level_id + 1;
+    let best_idx = picker_fn(hierarchy[`level_${other_level}`]);
+    hierarchy[`level_${level_id}`][agent_id] = {
+        age: hierarchy[`level_${other_level}`][best_idx]["age"],
+        competence: randomize_competence(
+            hierarchy[`level_${other_level}`][best_idx]["competence"],
+            competence_correlation
+        ),
+    };
+    replace_agent(other_level, best_idx);
+}
+
+function calculate_company_efficiency() {
+    let overall_competence = 0;
+    for (let level_id = 0; level_id < LEVELS; level_id += 1) {
+        let level_competence = 0;
+        for (
+            let agent_id = 0;
+            agent_id < hierarchy[`level_${level_id}`].length;
+            agent_id += 1
+        ) {
+            level_competence +=
+                hierarchy[`level_${level_id}`][agent_id]["competence"];
+        }
+        level_competence /= hierarchy[`level_${level_id}`].length;
+        overall_competence += level_competence * _level_weights[level_id];
+    }
+    return overall_competence / _total_level_weight;
+}
+
+function step() {
+    for (let level_id = LEVELS - 1; level_id >= 0; level_id -= 1) {
+        for (
+            let agent_id = 0;
+            agent_id < hierarchy[`level_${level_id}`].length;
+            agent_id += 1
+        ) {
+            let needs_replacement = false;
+
+            // age the agent
+            hierarchy[`level_${level_id}`][agent_id]["age"] += 1;
+
+            // retire the agent
+            if (
+                hierarchy[`level_${level_id}`][agent_id]["age"] >=
+                RETIREMENT_AGE
+            ) {
+                needs_replacement = true;
+            }
+
+            // remove incompetent
+            if (
+                hierarchy[`level_${level_id}`][agent_id]["competence"] <
+                required_competence
+            ) {
+                needs_replacement = true;
+            }
+
+            if (needs_replacement) {
+                replace_agent(level_id, agent_id);
+            }
+        }
+    }
+}
+
+function draw_hierarchy() {
+    const apply_cmap = (value) => {
+        value = Math.min(Math.max(value, 0), 1);
+        const lin_interp = (start, end, x) => start + (end - start) * x;
+        const low_color = [0, 0, 255]; // blue
+        const mid_color = [255, 255, 255]; // white
+        const high_color = [255, 0, 0]; // red
+        if (value < 0.5) {
+            // low-mid value range
+            const v = value / 0.5;
+            const r = Math.round(lin_interp(low_color[0], mid_color[0], v));
+            const g = Math.round(lin_interp(low_color[1], mid_color[1], v));
+            const b = Math.round(lin_interp(low_color[2], mid_color[2], v));
+            return `rgb(${r}, ${g}, ${b})`;
+        }
+        // mid-high value range
+        const v = (value - 0.5) / 0.5;
+        const r = Math.round(lin_interp(mid_color[0], high_color[0], v));
+        const g = Math.round(lin_interp(mid_color[1], high_color[1], v));
+        const b = Math.round(lin_interp(mid_color[2], high_color[2], v));
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+
+    let drawing_context = hierarchy_canvas.getContext("2d");
+
+    // clear the drawing
+    drawing_context.fillStyle = "#fff";
+    drawing_context.fillRect(
+        0,
+        0,
+        hierarchy_canvas.width,
+        hierarchy_canvas.height
+    );
+
+    // fill in the drawing
+    let block_size = 1;
+    for (let level_id = 0; level_id < LEVELS; level_id += 1) {
+        block_size = Math.pow(BRANCHING, LEVELS - 1 - level_id);
+        for (
+            let agent_id = 0;
+            agent_id < hierarchy[`level_${level_id}`].length;
+            agent_id += 1
+        ) {
+            drawing_context.fillStyle = apply_cmap(
+                hierarchy[`level_${level_id}`][agent_id]["competence"]
+            );
+            drawing_context.fillRect(
+                agent_id * block_size * HIERARCHY_BLOCK_WIDTH +
+                    HIERARCHY_LEFT_PAD,
+                _hierarchy_block_starts[level_id] + _hierarchy_top_pad,
+                HIERARCHY_BLOCK_WIDTH * block_size,
+                _hierarchy_block_heights[level_id]
+            );
+        }
+    }
+}
+
+function plot_competence() {
+    if (competence_history.length == 0) {
+        return;
+    }
+    const time = Array(competence_history.length)
+        .fill(null)
+        .map((v, i) => i + current_step - competence_history.length);
+    time_plot.update([time], [competence_history]);
+}
+
+function plot_histogram() {
+    if (competence_history.length == 0) {
+        return;
+    }
+    const prob_density = histogram.map((v, i) => {
+        let mult = 1;
+        if (i == 0 || i == histogram.length - 1) {
+            mult = 2;
+        }
+        return (mult * v) / current_step;
+    });
+    histogram_plot.update([HISTOGRAM_VALS], [prob_density]);
+}
+
+function update_plots() {
+    draw_hierarchy();
+    plot_competence();
+    plot_histogram();
+}
+
+function frame() {
+    if (continue_flag) {
+        let company_efficiency = 0;
+        for (let i = 0; i < STEPS_PER_FRAME; i += 1) {
+            step();
+            company_efficiency = calculate_company_efficiency();
+            competence_history.push(company_efficiency);
+            histogram[Math.round(company_efficiency / HISTOGRAM_BIN_SIZE)] += 1;
+        }
+        current_step += STEPS_PER_FRAME;
+        competence_history.splice(0, competence_history.length - MAX_HISTORY);
+
+        update_plots();
+
+        if (continue_flag) {
+            window.setTimeout(frame, FRAME_INTERVAL);
+        }
+    }
+}
+
+// events
+let start_btn = document.getElementById("start");
+start_btn.addEventListener("click", () => {
+    continue_flag = true;
+    start_btn.disabled = true;
+    level_multiplier_input.disabled = true;
+    competence_hypothesis_input.disabled = true;
+    promotion_algorithm_input.disabled = true;
+    restart_btn.disabled = false;
+    restart_btn.innerHTML = "Pause";
+    initialize();
+    frame();
+});
+
+let restart_btn = document.getElementById("restart");
+restart_btn.addEventListener("click", () => {
+    continue_flag = !continue_flag;
+    if (continue_flag) {
+        restore_simulation_values();
+        start_btn.disabled = true;
+        level_multiplier_input.disabled = true;
+        competence_hypothesis_input.disabled = true;
+        promotion_algorithm_input.disabled = true;
+        level_multiplier_input.disabled = true;
+        restart_btn.innerHTML = "Pause";
+        frame();
+    } else {
+        start_btn.disabled = false;
+        level_multiplier_input.disabled = false;
+        competence_hypothesis_input.disabled = false;
+        promotion_algorithm_input.disabled = false;
+        restart_btn.innerHTML = "Continue";
+    }
+});
+restart_btn.disabled = true;
+
+let level_multiplier_input = document.getElementById("level_multiplier");
+level_multiplier_input.addEventListener("change", () => {
+    update_level_multiplier();
+    update_plots();
+});
+
+let competence_hypothesis_input = document.getElementById(
+    "competence_hypothesis"
+);
+let promotion_algorithm_input = document.getElementById("promotion_algorithm");
+
+// main
+window.onload = () => {
+    initialize();
+    update_plots();
+};
